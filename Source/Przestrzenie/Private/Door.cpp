39dd6cad1fb7e6061bce3ca60e4fab4b291171d9
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2025-01-29 21:25:53 +0100

    Door WIP

---
+++ Source/Przestrzenie/Private/Door.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Door.h"
+#include "../PrzestrzeniePlayerController.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include <EnhancedInputComponent.h>
+
+// Sets default values
+ADoor::ADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	//RootComponent = Root;
+
+	Cube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cube"));
+	RootComponent = Cube;
+	UStaticMesh* CubeMesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("/Engine/BasicShapes/Cube.Cube")).Object;
+
+	Cube->SetStaticMesh(CubeMesh);
+	Cube->bCastDynamicShadow = true;
+	Cube->CastShadow = true;
+
+	Volume = CreateDefaultSubobject<UBoxComponent>(TEXT("Volume"));
+	Volume->SetupAttachment(Root);
+	Volume->InitBoxExtent(FVector(100.0f, 100.0f, 100.0f));
+	Volume->SetCollisionProfileName(TEXT("OverlapAll"));
+
+	RotationSpeed = 2.0f;
+	IsRotating = false;
+	RotationAlpha = 0.0f;
+	IsOpen = false;
+
+}
+
+void ADoor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ADoor::Interact()
+{
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (PlayerController)
+	{
+		APrzestrzeniePlayerController* PC = Cast<APrzestrzeniePlayerController>(PlayerController);
+		if (PC->ItemArray.Contains(1)) //1 is the id of a key
+		{
+			IsRotating = true;
+		}
+	}
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartRotation = GetActorRotation();
+	
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsRotating && !IsOpen)
+	{
+		RotationAlpha += DeltaTime * RotationSpeed;
+		if (RotationAlpha >= 1.0f)
+		{
+			RotationAlpha = 1.0f;
+			IsRotating = false;
+			IsOpen = true;
+		}
+
+		FRotator NewRotation = FRotator(StartRotation.Pitch, FMath::Lerp(StartRotation.Yaw, TargetRotation.Yaw, RotationAlpha), StartRotation.Roll);
+		SetActorRotation(NewRotation);
+	}
+
+}
+
+void ADoor::Rotate()
+{
+
+}
+
